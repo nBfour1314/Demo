1b015a3df9a410e04bf3c6389b462ee5b8868986
nBfour1314 <91709361+nBfour1314@users.noreply.github.com>
2021-10-23 09:47:27 +0700

    Update Connection.php

--- Connection.php
+++ Connection.php
@@ -2,6 +2,6 @@
     $conn = pg_connect("postgres://fducuqddsvgmah:e60411575ecbca5d71b672a3eee05935aa4f6a240d0896c57ed6ac2141a1410b@ec2-18-234-15-247.compute-1.amazonaws.com:5432/dqp0onvttuqbr");
 	
     if (!$conn) {
-        die echo "Connection failed";
+        echo die "Connection failed";
     }
 ?>
