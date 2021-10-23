a409b0c8b1925bfbe1031933cce0faf32d7b0ed7
nBfour1314 <nguyennhathao11c2@gmail.com>
2021-10-23 09:18:30 +0700

    code updated 3

--- Connection.php
+++ Connection.php
@@ -1,7 +1,7 @@
 <?php
-    $Connect = pg_connect("postgres://fducuqddsvgmah:e60411575ecbca5d71b672a3eee05935aa4f6a240d0896c57ed6ac2141a1410b@ec2-18-234-15-247.compute-1.amazonaws.com:5432/dqp0onvttuqbr");
+    $conn = pg_connect("postgres://fducuqddsvgmah:e60411575ecbca5d71b672a3eee05935aa4f6a240d0896c57ed6ac2141a1410b@ec2-18-234-15-247.compute-1.amazonaws.com:5432/dqp0onvttuqbr");
 	
-    if (!$Connect) {
+    if (!$conn) {
         die("Connection failed");
     }
 ?>
